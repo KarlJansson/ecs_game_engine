8f70264995e188e0813de86875d00f9e5525f810
Karl Jansson <karljansson@localhost.localdomain>
2018-05-26 15:08:22 +0200

    - added build and cooking tool copy to build unix script

--- build_cmake_unix.sh
+++ build_cmake_unix.sh
@@ -5,6 +5,8 @@ mkdir _build_release_
 cd _build_release_
 CXX=clang++ cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_USER_MAKE_RULES_OVERRIDE=~/ClangOverrides.txt -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 cp compile_commands.json ..
+make -j
+sudo cp ./cmd_fract_cooking/Build_Output/bin/fract_cooking /usr/bin
 
 cd ..
 rm -rf ./_build_debug_
